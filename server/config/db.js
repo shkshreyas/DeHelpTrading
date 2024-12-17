93803646c91bc8f41659b20c0b22c5b78ab25e06
Shreyas Kumar <shreyaskumar467@gmail.com>
2024-12-17 05:32:46 +0530

    first

---
+++ server/config/db.js
@@ -0,0 +1,25 @@
+// /config/db.js
+
+const mongoose = require('mongoose');
+
+// Hardcoded MongoDB URI
+const db = "mongodb://localhost:27017/TNTCharts";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
